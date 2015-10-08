7fcf842051c3ef595d163b6ca9c0e0d25d0ec73c
Richard Tan <chardos@gmail.com>
2015-10-08 23:47:37 +1100

    First commit.. woot!!!

---
+++ www/js/app.js
@@ -0,0 +1,58 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider.state('home', {
+    url: '/',
+    templateUrl: 'index.html'
+  })
+
+  $stateProvider.state('add', {
+    url: '/add',
+    templateUrl: 'add.html'
+  })
+})
+
+.controller('ListCtrl', function($scope) {
+  $scope.properties = [];
+  for (var i = 0; i < localStorage.length; i++){
+    var parsed = JSON.parse( localStorage.getItem(localStorage.key(i)) );
+    $scope.properties.push( parsed );
+    console.log(parsed.address);
+    //console.log( localStorage.getItem( localStorage.key(i) ));
+  }
+})
+
+.controller('ShowCtrl', function($scope) {
+  // Close the new task modal
+  $scope.saveProperty = function() {
+    var property = {
+      address: document.getElementById('address-input').value,
+      price: document.getElementById('price-input').value,
+      body_corp: document.getElementById('body-corp-input').value,
+      rating: document.getElementById('rating-input').value,
+      notes: document.getElementById('notes-input').value
+    }
+    window.localStorage[property.address] = JSON.stringify(property);
+  };
+});
+
