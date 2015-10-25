87adf582ff38c1fccdd929187df4bdd4a8036239
Richard Tan <chardos@gmail.com>
2015-10-25 14:53:10 +1100

    Refactor listing sync, + load refresh view on database retrieve

--- www/js/controllers/list.js
+++ www/js/controllers/list.js
@@ -1,13 +1,25 @@
 houseHunter.controller('ListCtrl', function($scope, $state) {
 
   $scope.syncLocalToDb = function(){
+    console.log('SYNCING FROM DB');
     localStorage.clear();
-      console.log('SYNCING FROM DB');
+    $scope.properties = [];
+    myDataRef.on("value", function(snapshot) { //retrieve from db
+      var properties = snapshot.val()
+      for (var key in properties) {
+        var property = properties[key];
+        window.localStorage[property.address] = JSON.stringify(property);
+        console.log('SHOWING DB');
+        console.log(property);
+        $scope.properties.push( property );
+        $state.go('list');
+      }
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+
     //replace local storage
-    for (var key in $scope.properties) {
-      var property = $scope.properties[key];
-      window.localStorage[property.address] = JSON.stringify(property);
-    }
+
   }
   $scope.showLocalProperties = function(){
     $scope.properties = [];
@@ -20,22 +32,7 @@ houseHunter.controller('ListCtrl', function($scope, $state) {
       console.log('SHOWING LOCAL');
     }
   }
-  $scope.showDatabaseProperties = function(){
-    $scope.properties = [];
-    myDataRef.on("value", function(snapshot) { //retrieve from db
-      var properties = snapshot.val()
-      for (var key in properties) {
-        var property = properties[key];
-        console.log('SHOWING DB');
-        console.log(property);
-        $scope.properties.push( property );
-      }
-      $scope.syncLocalToDb();
 
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
-    });
-  }
 
   $scope.showLocalProperties();
 
@@ -43,7 +40,7 @@ houseHunter.controller('ListCtrl', function($scope, $state) {
 
 
   $scope.doRefresh = function(){
-    $scope.showDatabaseProperties();
+    $scope.syncLocalToDb();
     $scope.$broadcast('scroll.refreshComplete');
   }
   $scope.deleteProperty = function(item){
