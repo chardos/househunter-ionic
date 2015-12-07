426f1d01c495cba2456972d7ea5c92c7442283a4
Richard Tan <chardos@gmail.com>
2015-12-08 00:50:28 +1100

    Split routes out into separate file

---
+++ www/app/app.route.js
@@ -0,0 +1,45 @@
+houseHunter.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state('login', {
+    url: '/login',
+    templateUrl: 'app/components/login/loginView.html',
+    cache: false
+  })
+  $stateProvider.state('register', {
+    url: '/register',
+    templateUrl: 'app/components/register/registerView.html',
+    cache: false
+  })
+  $stateProvider.state('list', {
+    url: '/list',
+    templateUrl: 'app/components/list/listView.html',
+    cache: false
+  })
+
+  $stateProvider.state('detail', {
+    url: '/list/:address',
+    templateUrl: 'show.html',
+    cache: false
+  })
+
+  $stateProvider.state('add', {
+    url: '/add',
+    templateUrl: 'app/components/add/addView.html',
+    cache: false
+  })
+
+  $stateProvider.state('edit', {
+    url: '/edit/:address',
+    templateUrl: 'edit.html',
+    cache: false
+  })
+
+  $stateProvider.state('compass', {
+    url: '/compass',
+    templateUrl: 'app/components/compass/compassView.html'
+  })
+
+  //$urlRouterProvider.otherwise('/list')
+  $urlRouterProvider.otherwise('/login')
+
+})
