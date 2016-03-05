bf698d5e46ca539a6a8b235e94b14d401df853c8
Richard Tan <chardos@gmail.com>
2016-03-05 21:00:40 +1100

    Show next 4 weeks of events (and turn on using real data)

--- www/app/components/events/eventsController.js
+++ www/app/components/events/eventsController.js
@@ -3,8 +3,8 @@ houseHunter.controller('EventsCtrl', function($scope, $state, eventsService) {
 
   var dummyData = [{"calendar_id":"9","title":"Buddy's flea drop","dtstart":1456866000000,"dtend":1456869600000,"eventLocation":"","allDay":0},{"calendar_id":"9","title":"Web Development Immersive Interview","dtstart":1456959600000,"dtend":1456961400000,"eventLocation":"General Assembly Campus, Level 12A, 45 William Street, Melbourne 3000","allDay":0},{"calendar_id":"9","title":"Inspection","dtstart":1457139600000,"dtend":1457141400000,"eventLocation":"107 Gladstone Avenue, Northcote","allDay":0},{"calendar_id":"9","title":"Inspection","dtstart":1457143200000,"dtend":1457145000000,"eventLocation":"11/52 Gadd Street, Northcote","allDay":0},{"calendar_id":"9","title":"Buddy's birthday ","dtstart":1457211600000,"dtend":1457215200000,"eventLocation":"","allDay":0},{"calendar_id":"11","title":"Labour Day (Western Australia)","dtstart":1457308800000,"dtend":1457395200000,"eventLocation":"","allDay":1},{"calendar_id":"9","title":"Flight to Singapore","dtstart":1457314200000,"dtend":1457342700000,"eventLocation":"Melbourne MEL","allDay":0},{"calendar_id":"9","title":"Inspection","dtstart":1457591400000,"dtend":1457593200000,"eventLocation":"107 Gladstone Avenue, Northcote","allDay":0},{"calendar_id":"11","title":"Canberra Day (Australian Capital Territory)","dtstart":1457913600000,"dtend":1458000000000,"eventLocation":"","allDay":1},{"calendar_id":"11","title":"Adelaide Cup (South Australia)","dtstart":1457913600000,"dtend":1458000000000,"eventLocation":"","allDay":1},{"calendar_id":"11","title":"Labour Day (Victoria)","dtstart":1457913600000,"dtend":1458000000000,"eventLocation":"","allDay":1},{"calendar_id":"11","title":"Eight Hours Day (Tasmania)","dtstart":1457913600000,"dtend":1458000000000,"eventLocation":"","allDay":1}]
 
-  var startDate = new Date(2016,2,1,18,30,0,0,0); // beware: month 0 = january, 11 = december
-  var endDate = new Date(2016,2,15,19,30,0,0,0);
+  var startDate = moment().startOf('day').toDate(); // beware: month 0 = january, 11 = december
+  var endDate = moment().add(4, 'weeks').endOf('day').toDate();
   var title = "My nice event";
   var eventLocation = "Home";
   var notes = "Some notes about this event.";
@@ -14,16 +14,18 @@ houseHunter.controller('EventsCtrl', function($scope, $state, eventsService) {
 
   var success = function(events) {
     var filteredEvents = eventsService.filterEvents(events, "Inspection");
+    filteredEvents = eventsService.addDatesToObj(filteredEvents);
     $scope.events = filteredEvents;
   };
   // $scope.events = [];
 
-  var filteredEvents = eventsService.filterEvents(dummyData, "Inspection");
-  filteredEvents = eventsService.addDatesToObj(filteredEvents);
-  $scope.events = filteredEvents;
+  //DEV STUFF
+  // var filteredEvents = eventsService.filterEvents(dummyData, "Inspection");
+  // filteredEvents = eventsService.addDatesToObj(filteredEvents);
+  $scope.events = [];
 
   var error = function(message) { alert("Error: " + message); };
 
-  //window.plugins.calendar.listEventsInRange(startDate,endDate,success,error);
+  window.plugins.calendar.listEventsInRange(startDate,endDate,success,error);
 
 })
